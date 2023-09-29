15f6d93907c2b8d6a08d6c77a94fea1766e4eca0
ramajoballester <alvaroramajo1@gmail.com>
2023-09-29 12:27:09 +0200

    Automate docs generation

---
+++ docs/create_docs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define input and output file paths
+input_file="./README.md"
+output_file="./docs/source/readme.md"
+
+# Use grep to filter link lines and sed to remove them
+grep -v '^![^)]*)' "$input_file" > "$output_file"
+
+echo "Filtered $input_file saved to $output_file"
+
+# Build the docs locally
+sphinx-build -M html docs/source/ docs/build/ -a
